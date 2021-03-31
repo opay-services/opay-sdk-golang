7cf42936a270760c339fca48a399fda48ba94fc1
jian.li <lijian2233@163.com>
2021-03-31 11:00:47 +0800

    readme

---
+++ readme.mk
@@ -0,0 +1,10 @@
+#Introduction
+OPay provides easy-to-use checkout integration with OPay Checkout as well as generic and powerful RESTful APIs which allow merchants to solve most of their payment use cases.
+
+The Cashier and Transaction APIs allow merchants to accept payments from end user into the merchant's OPay wallet.
+
+The Transfer APIs allow merchants to transfer money to another OPay users' wallet.
+
+The Charge APIs allow merchants to charge its own wallet with Bank Card.
+
+#
